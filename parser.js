0c312ca0deab3a65614f1421776803de0e21f7d7
aiden <aiden.t.oneal@gmail.com>
2024-05-21 09:04:17 -0500

    Update parser.js

--- parser.js
+++ parser.js
@@ -1,29 +1,33 @@
+// Validate ISRC string
+function isrcValidate(isrc) {
+    const isrcRegex = new RegExp(/^[A-Z]{2}[A-Z0-9]{3}\d{7}$/);
+    // Return boolean
+    return isrcRegex.test(isrc);
+};
+
 // Parse incoming flat ISRC
 function isrcParser(isrc) {
-    let parsedIsrc = {
+    if (!isrcValidate(isrc)) throw new Error("Invalid 'isrc'.");
+    // Return object
+    return {
         country: isrc.slice(0, 2),
         registrant: isrc.slice(2, 5),
         year: Number(isrc.slice(5, 7)),
         designation: Number(isrc.slice(7, 12))
     };
-    return parsedIsrc;
 };
 
 // Parse ISRC object to flat string
 function isrcToString(isrc) {
-    if (typeof isrc !== "object" || Array.isArray(isrc) || isrc === null) {
-        return console.log("Error; 'isrc' should be an object.");
-    };
-    let isrcString = isrc.country + isrc.registrant + isrc.year + isrc.designation.toString().padStart(5, "0");
-    return isrcString;
+    if (typeof isrc !== "object" || Array.isArray(isrc) || isrc === null) throw new Error("Parameter should be an object.");
+    // Return string
+    return `${isrc.country}${isrc.registrant}${isrc.year}${isrc.designation.toString().padStart(5, "0")}`
 }
 
 // Generate new ISRC object
 function isrcGenerate(country, registrant, year, existingIsrcs) {
     // Error handling
-    if (!Array.isArray(existingIsrcs)) {
-        return console.log("Error; 'existingIsrcs' should be an array.")
-    };
+    if (!Array.isArray(existingIsrcs)) throw new Error("Parameter should be an array.");
     // Create temporary sorted array with all ISRCs that match params
     let existingIsrcsSorted = [];
     existingIsrcs.forEach(e => {
@@ -34,24 +38,24 @@ function isrcGenerate(country, registrant, year, existingIsrcs) {
     });
     // Increment designation
     let designation = Number(Math.max(...existingIsrcsSorted)) + 1;
-    // Define object
-    const newIsrc = {
+    if (designation > 99999) throw new Error("Max designation reached.")
+    // Return object
+    return {
         country: country,
         registrant: registrant,
         year: year,
         designation: designation,
     };
-    // Write ISRC as string
-    let newIsrcString = isrcToString(newIsrc);
-    // Log in console
-    console.log(newIsrc);
-    console.log(newIsrcString)
 };
 
 // Dummy data
-let isrc = "QZZDS2400001";
-let existingIsrcs = ["QZZDS2400001","QZZDS2400002","QZZDS2400003"]
+let isrcValid = "QZZDS2400001";
+let isrcInalid = "QZZDS240000A";
+
+let existingIsrcs = ["QZZDS2400001","QZZDS2400002","QZZDS2499998"]
 let currentYear = Number(new Date().getFullYear().toString().slice(2, 4));
 
 // Run
-isrcGenerate("QZ", "ZDS", currentYear, existingIsrcs)
+// isrcGenerate("QZ", "ZDS", currentYear, existingIsrcs)
+// isrcParser(isrcValid)
+// console.log(isrcGenerate("QZ", "ZDS", currentYear, existingIsrcs))
